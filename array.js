c30005339079ae9eff9c709be30ef6e96d581645
Husan9898 <husanpreetsingh.lnu@rdpolytech.ca>
2025-06-05 09:34:19 -0600

    update

---
+++ array.js
@@ -0,0 +1,15 @@
+//array literal
+//array are 0-based indexed
+
+var array =['apple', 2.33, null, true, 'dog', 'cat', 23,6];
+
+console.log(array[4]); //dog
+
+//reassignment of value
+array[4] = 'cat';
+
+console.log(array[4]); // output: cat
+
+//call length on the array
+console.log(array.length); //output: 8
+
